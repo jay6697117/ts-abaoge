edec97cd542a4488dd14b5ab8534fbf942f93fa5
steve zhang <871506263@qq.com>
2021-07-28 02:04:32 +0800

    提交代码于: 2021-07-28 02:04:30

---
+++ src/001.js
@@ -0,0 +1,10 @@
+function greet(person) {
+    return 'Hello, ' + person;
+}
+console.log(greet('TypeScript'));
+var Person = /** @class */ (function () {
+    function Person() {
+        var name = 'aaaa';
+    }
+    return Person;
+}());
---
+++ src/001.ts
@@ -0,0 +1,11 @@
+function greet(person: string) {
+  return 'Hello, ' + person;
+}
+
+console.log(greet('TypeScript'));
+
+class Person {
+  constructor() {
+    let name: string = 'aaaa';
+  }
+}
