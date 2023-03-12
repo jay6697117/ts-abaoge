732ded592ea82d0e86219ec3dc281341772f6a67
steve zhang <871506263@qq.com>
2023-03-12 20:59:15 +0800

    zhangjinhui提交代码于: 2023-03-12 20:59:15

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
+const fs = require('fs')
+console.log('fs', fs)
+console.log(fs.readdir());
+
 import './lesson/001';
--- src/lesson/001.ts
+++ src/lesson/001.ts
@@ -1,4 +1,1 @@
 export default  {};
-
-const aaa:any = '1111'
-console.log('aaa', aaa)
