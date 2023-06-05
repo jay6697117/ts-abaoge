05f42e636cac9eb89c1dacd577681cb776ed3acd
steve zhang <871506263@qq.com>
2023-06-05 15:27:44 +0800

    zhangjinhui提交代码于: 2023-06-05 15:27:44

--- src/test.js
+++ src/test.js
@@ -32,3 +32,75 @@ console.log('result', result)
 let html = document.querySelector('.elem')?.innerHTML; // 如果 document.querySelector('.elem') 的结果为 null，则会出现错误
 console.log('html', html);
  */
+
+// let user = {address:{street:1}}; // user 没有 address 属性
+// console.log(user?.address?.street); // undefined（不报错）
+
+/*
+let user = null;
+console.log(user); // null
+console.log(user?.address); // undefined
+console.log(user?.address.street); // undefined
+ */
+/*
+let user = null;
+let x = 0;
+console.log(user?.sayHi(x++)); // undefined 没有 "user"，因此代码执行没有到达 sayHi 调用和 x++
+console.log(x);; // 0，值没有增加
+ */
+/*
+let userAdmin = {
+  admin() {
+    return "I am admin";
+  }
+};
+let userGuest = {};
+
+console.log(userAdmin.admin?.());; // I am admin
+console.log(userGuest.admin?.());; // undefined 啥都没发生（没有这样的方法）
+ */
+
+// const obj = {
+//   a:'a-111',
+//   b:'b-222'
+// }
+// const key = 'a';
+// console.log(obj.a);
+// console.log(obj[key]);
+/*
+// 可选链 "?."
+let key = 'firstName';
+let user1 = {
+  firstName: 'John'
+};
+let user2 = null;
+
+console.log(user1?.[key]); // John
+console.log(user2?.[key]); // undefined
+
+let user = {
+  info: {
+    name: 'zjh',
+    age: 18
+  },
+  info1: null
+};
+delete user.info?.name; // 如果 user.info 存在，则删除 user.info.name
+delete user.info1?.name; // 如果 user.info 存在，则删除 user.info.name
+console.log('user', user);
+ */
+
+// 我们可以使用 ?. 来安全地读取或删除，但不能写入
+// 可选链 ?. 不能用在赋值语句的左侧。
+/*
+let user = null;
+user?.name = "John"; // Error，不起作用
+// 因为它在计算的是：undefined = "John"
+console.log('user', user)
+ */
+
+// 总结
+// 可选链 ?. 语法有三种形式：
+// obj?.prop —— 如果 obj 存在则返回 obj.prop，否则返回 undefined。
+// obj?.[prop] —— 如果 obj 存在则返回 obj[prop]，否则返回 undefined。
+// obj.method?.() —— 如果 obj.method 存在则调用 obj.method()，否则返回 undefined。
